c5141c241b8f769a70723b1f1e6d777094fc764b
appuser <appuser@journey2-86bff4fbf7-5gdcq>
2023-07-27 06:41:35 +0000

    starter project

---
+++ Codecool.LanguageInternalization/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Codecool.LanguageInternalization;
+
+class Program
+{
+    private static readonly string WorkDir = AppDomain.CurrentDomain.BaseDirectory;
+    private static readonly char[] SpecialCharacters = { 'ö', 'ü', 'ä', 'ß' };
+
+    public static void Main(string[] args)
+    {
+        var file = $"{WorkDir}\\Resources\\german-example.txt";
+
+        PrintSpecialCharacterStatistics(file);
+        WriteLinesWithSpecialCharacters(file);
+        WriteInternationalizedText(file);
+    }
+
+    private static void PrintSpecialCharacterStatistics(string file)
+    {
+    }
+
+
+    private static void WriteLinesWithSpecialCharacters(string file)
+    {
+    }
+
+    private static void WriteInternationalizedText(string file)
+    {
+    }
+}
